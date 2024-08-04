f3ef855f445b3f4be4673770cb90482aa81169e3
sergio-eso2 <super.sergio@icloud.com>
2024-08-03 22:31:42 -0300

    Add files via upload

---
+++ prova infinity2.py
@@ -0,0 +1,14 @@
+numero1 = float(input("Digite um numero: "))
+
+if numero1 > 0:
+    print(f"O {numero1} é um numero positivo")
+
+elif numero1 == 0:
+    print("O numero é 0")
+
+else:
+    print(f"O {numero1} é um numero negativo")
+
+    
+
+
